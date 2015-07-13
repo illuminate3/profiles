1d4155186bde02eaf0fb509d787d93b0a968b7a3
illuminate3 <carsten.sapporo@gmail.com>
2015-07-12 19:20:33 -0500

    dev 4.2

--- Resources/Views/welcome/profiles.blade.php
+++ Resources/Views/welcome/profiles.blade.php
@@ -21,7 +21,7 @@
 	<div class="container">
 		<div class="content">
 			<a href="/">
-				<img src="/assets/images/usr.png">
+				<img src="/assets/images/usr.png" class="img-responsive">
 			</a>
 			<div class="title">
 				<a href="/">
